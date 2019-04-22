4ee2eda63d27bb8c881e2297d06c6bd71aa3f77e
nicholas husher <nhusher@gmail.com>
2019-04-22 17:47:03 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const { createEventAdapter } = require('@slack/events-api')
+const { WebClient } = require('@slack/web-api')
+const {
+  SLACK_SIGNING_SECRET,
+  SLACK_TOKEN,
+  PORT = 3000
+} = process.env
+
+const slackEvents = createEventAdapter(SLACK_SIGNING_SECRET)
+const webClient = new WebClient(SLACK_TOKEN)
+
+slackEvents.on('message', event => {
+  if (event.type === 'team_join') {
+    webClient.chat.postMessage({
+      text: `User ${event.user.name} signed up`,
+      channel: 'UG691UQA1'
+    })
+  }
+})
+
+(async () => {
+  const server = await slackEvents.start(PORT)
+  console.log(`Listening for events on ${server.address().port}`)
+})()
